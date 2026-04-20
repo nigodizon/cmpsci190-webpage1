0ffcaaf148f529150535e9a3f8a451afe607e135
Isaiah Dizon <nigodizon9@gmail.com>
2026-04-20 01:43:45 -0700

    Lab #3

--- student.js
+++ student.js
@@ -1,6 +1,7 @@
-thename = "Isaiah Dizon";
-info2 = "cs190 Javascript Lab 2";
-id2 = "865";
+var thename = "Isaiah Dizon";
+var info2 = "cs190 JavaScript Lab 2";
+var id2 = "865";
+var special = 1;
 
 function displayinfo375() {
   alert(thename + " " + info2 + " " + id2);
@@ -8,4 +9,24 @@ function displayinfo375() {
 
 function confirmlink276() {
   return confirm("Are you sure you want to leave this page?");
+}
+
+function displayspecial617() {
+  if (special == 1) {
+    alert("The current special is #1");
+  }
+
+  if (special == 2) {
+    alert("The current speical is #2");
+  }
+
+  if (special == 3) {
+    alert("The current special is #3");
+  }
+
+  special = special + 1;
+
+  if (special > 3) {
+    special = 1;
+  }
 }
