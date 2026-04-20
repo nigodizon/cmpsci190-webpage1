7c3543229f9290615514172d1c8f593d99545119
Isaiah Dizon <nigodizon9@gmail.com>
2026-04-19 22:42:43 -0700

    Fix spacing in alert message

--- student.js
+++ student.js
@@ -3,7 +3,7 @@ info2 = "cs190 Javascript Lab 2";
 id2 = "865";
 
 function displayinfo375() {
-  alert(thename + "\n" + info2 + "\n" + id2);
+  alert(thename + " " + info2 + " " + id2);
 }
 
 function confirmlink276() {
