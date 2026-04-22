c9e299f4fd6bd8a91491022a6ad2dfcafa32c571
Isaiah Dizon <nigodizon9@gmail.com>
2026-04-21 22:50:04 -0700

    Lab 4 Commits

--- student.js
+++ student.js
@@ -2,6 +2,7 @@ var thename = "Isaiah Dizon";
 var info2 = "cs190 JavaScript Lab 2";
 var id2 = "865";
 var special = 1;
+var thetype = "direct";
 
 function displayinfo375() {
   alert(thename + " " + info2 + " " + id2);
@@ -28,5 +29,38 @@ function displayspecial617() {
 
   if (special > 3) {
     special = 1;
+  }
+}
+
+function changetype607() {
+  var newtype;
+
+  if (thetype == "direct") {
+    newtype = "advertising";
+  }
+
+  if (thetype == "advertising") {
+    newtype = "subscription";
+  }
+
+  if (thetype == "subscription") {
+    newtype = "direct";
+  }
+
+  thetype = newtype;
+  alert("Customer type changed to " + thetype);
+}
+
+function displaytype714() {
+  if (thetype == "direct") {
+    alert("Buy Now! From this web page! My children need new shoes!");
+  }
+
+  if (thetype == "advertising") {
+    alert("Support our advertisers! Click on an ad, so I can make money!");
+  }
+
+  if (thetype == "subscription") {
+    alert("Renew your subscription today! My children need medicine!");
   }
 }
