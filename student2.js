b874548d1decc7ae78ddae09fb0db4c590553058
Isaiah Dizon <nigodizon9@gmail.com>
2026-05-06 17:20:48 -0700

    Lab #8

--- student2.js
+++ student2.js
@@ -4,16 +4,21 @@ phonenumber5 = "(555) 638-1234";
 
 function getHeader() {
   var output;
+  var now;
+
+  now = new Date();
 
   output = "";
   output =
     output +
-    "<div style='width:100%; background-color:lightblue; text-align:center; padding:20px;'>";
+    "<div style='width:100%; background-color:lightblue; padding:20px;'>";
+  output =
+    output + "<div style='text-align:right; font-size:12px;'>" + now + "</div>";
   output =
     output +
-    "<span style='font-size:36px; font-weight:bold;'>" +
-    companyname4 +
-    "</span>";
+    "<div style='text-align:center; font-size:36px; font-weight:bold;'>";
+  output = output + companyname4;
+  output = output + "</div>";
   output = output + "</div>";
 
   return output;
@@ -26,10 +31,46 @@ function getFooter(companyname, address, phonenumber) {
   output =
     output +
     "<div style='width:100%; background-color:lightgray; text-align:center; padding:12px; margin-top:30px;'>";
-  output = output + "<span style='font-size:14px;'>";
-  output = output + companyname + " | " + address + " | " + phonenumber;
+  output = output + "<span style='font-size:12px;'>";
+  output = output + companyname + " | " + address + " | " + phonenumber + " | ";
+  output =
+    output +
+    navigator.appName +
+    " | " +
+    navigator.appVersion +
+    " | " +
+    navigator.platform;
   output = output + "</span>";
   output = output + "</div>";
+
+  return output;
+}
+
+function makeMenu2(size) {
+  var output;
+  var count;
+
+  output = "My Menu: ";
+  count = 0;
+
+  while (count < size) {
+    output = output + "Button ";
+    count = count + 1;
+  }
+
+  return output;
+}
+
+function makeMain9() {
+  var output;
+
+  output = "";
+  output = output + "My Product:<br>";
+  output = output + "Product Name: <br>";
+  output = output + "Product ID: <br>";
+  output = output + "Product Description: <br>";
+  output = output + "Shopping Cart Link Bar <br>";
+  output = output + "Product Image: <br>";
 
   return output;
 }
